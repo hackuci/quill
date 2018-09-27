689cb3b202d9a0bf83fece33e6fbbe545f75f583
kbuci <krishenb@uci.edu>
2018-09-26 22:54:46 -0700

    tested testSendGridTemplates.js with ENV variables

--- scripts/testSendGridTemplates.js
+++ scripts/testSendGridTemplates.js
@@ -1,8 +1,8 @@
 // Script to test every (dynamic) SendGrid email template with SendGrid email API
 // The env email address should recieve every template type defined in ENV (sent from itself)
 require('dotenv').load();
-var Mailer = require('../app/server/services/sendgrid_email');
 
+var Mailer = require('../app/server/services/sendgrid_email');
 var email = process.env.EMAIL_ADDRESS;
 var templates = ['verification','passwordChanged','passwordReset','accepted', 'waiver'];
 var mailCheck = function( templateIndex ) 
