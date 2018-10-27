708796c9d2cf3a386b6d16fd3b07071190c8d374
Chris Wong <chris@cogifire.com>
2018-10-27 15:29:37 -0700

    Update scripts/testSendGridTemplates.js
    Co-Authored-By: kbuci <krishenb@uci.edu>

--- scripts/testSendGridTemplates.js
+++ scripts/testSendGridTemplates.js
@@ -6,7 +6,7 @@ var Mailer = require('../app/server/services/sendgrid_email');
 var email = process.env.EMAIL_ADDRESS;
 var templates = ['verification','passwordChanged','passwordReset','accepted', 'waiver'];
 var mailCheck = function (templateIndex) {
-    var ret = function(err, data) {
+    var ret = function (err, data) {
         if(!err && data) {
             console.warn('Email for template ' + templates[templateIndex] + ' sent');
         }
