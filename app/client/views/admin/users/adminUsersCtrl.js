d1ddca73824dcfb6dc3fa39fa03ee73ce1ff7409
kbuci <krishenb@uci.edu>
2018-12-15 01:45:59 -0800

    Trying to sort essay len in query: 1

--- app/client/views/admin/users/adminUsersCtrl.js
+++ app/client/views/admin/users/adminUsersCtrl.js
@@ -10,7 +10,7 @@ angular.module('reg')
 
       $scope.pages = [];
       $scope.users = [];
-      $scope.sortMode = {'profile.essay.length' : 'desc'};
+      $scope.sortMode = {'$strLenCP' : 'profile.essay'};
       // Semantic-UI moves modal content into a dimmer at the top level.
       // While this is usually nice, it means that with our routing will generate
       // multiple modals if you change state. Kill the top level dimmer node on initial load
